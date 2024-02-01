2bf1cc886716e00e7f90134f284e3f9916fa743d
Nigel <nigelfernandes.contact@gmail.com>
2024-02-01 12:27:35 +0530

    change App1.java

--- App1.java
+++ App1.java
@@ -1,5 +1,8 @@
 public class App1 {
     public static void main(String[] args) {
         System.out.println("Java App Pulled & Build by Jenkins");
+        System.out.println("==================================");
+        System.out.println("New changes...");
+        System.out.println("==================================");
     }
 }
